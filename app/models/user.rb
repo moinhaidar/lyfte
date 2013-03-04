005362c069fc2d30d9d05c07cf0d617d681be4a5
Moin Haidar <moinhaidar@gmail.com>
2013-03-04 18:59:50 +0530

    Annotating Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  name                   :string(255)
+#  address                :text
+#  age                    :integer
+#  sex                    :string(255)
+#  mobile                 :string(255)
+#  referer                :string(255)
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  salt                   :string(255)
+#  registeration_token    :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 class User < ActiveRecord::Base
   
   # Setup accessible (or protected) attributes for your model
