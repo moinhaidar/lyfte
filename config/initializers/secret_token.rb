eca7c3a0b41197057b83a7053f5969e999a24074
Moin Haidar <moinhaidar@gmail.com>
2013-03-04 11:26:23 +0530

    initializing app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lyfte::Application.config.secret_token = '452b58efd2e0b2728b598ae0a6af91842280e06d52c8fd117740ed7d1b791fa5e618262de702d436cb39da2a07522d02c2085185fcaabe3e2e8d0e951f95cea6'
